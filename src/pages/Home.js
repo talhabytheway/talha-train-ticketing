5aefbe8e74c8d35d1f4ab743a019b780aa28db97
Muhammad Talha <talhahanfsd@gmail.com>
2023-05-01 22:23:15 +0500

    Add Card Carousels

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,11 +18,20 @@ import Hamburger from '../assets/svg/Hamburger';
 import hand from '../assets/images/hand.png';
 import MainCard from '../components/home/MainCard';
 import TicketSeya from '../components/home/TicketSeya';
+import Berita from '../components/home/Berita';
 
 const Home = prop => {
   let ticketSeya = [
     {time: 'Besok', location: 'Bengawan', route: 'PWT - LPY'},
     {time: '7 hari', location: 'Bims', route: 'YK - PWT'},
+    {time: 'Besok', location: 'Bengawan', route: 'PWT - LPY'},
+  ];
+
+  let berita = [
+    ['Tips', 'Tetap jaga komunikasi selama di kereta'],
+    ['Update', 'Protokol kesehatan di kereta'],
+    ['Tips', 'Tetap jaga komunikasi selama di kereta'],
+    ['Update', 'Protokol kesehatan di kereta'],
   ];
   return (
     <ImageBackground style={styles.bg} source={bg} resizeMode="stretch">
@@ -30,19 +39,37 @@ const Home = prop => {
         <View style={styles.ham}>
           <Hamburger />
         </View>
-        <View style={styles.mainPar}>
-          <Text style={styles.mainHead}>Mau pergi ke mana kali ini?</Text>
-          <Image source={hand} style={styles.hand} />
-        </View>
-        <MainCard />
-        <View style={styles.tickSeyPar}>
-          <Text style={styles.tickSey}>Tiket saya</Text>
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            {ticketSeya.map((e, index) => (
-              <TicketSeya {...e} index={index} />
-            ))}
-          </ScrollView>
-        </View>
+        <ScrollView>
+          <View style={styles.mainPar}>
+            <Text style={styles.mainHead}>Mau pergi ke mana kali ini?</Text>
+            <Image source={hand} style={styles.hand} />
+          </View>
+          <MainCard {...prop} />
+          <View style={styles.tickSeyPar}>
+            <Text style={styles.tickSey}>Tiket saya</Text>
+            <ScrollView
+              contentContainerStyle={{paddingLeft: 8}}
+              contentInset={{left: -8}}
+              horizontal
+              showsHorizontalScrollIndicator={false}>
+              {ticketSeya.map((e, index) => (
+                <TicketSeya key={index} {...e} index={index} />
+              ))}
+            </ScrollView>
+          </View>
+          <View style={styles.beritaPar}>
+            <Text style={styles.tickSey}>Berita</Text>
+            <ScrollView
+              contentContainerStyle={{paddingLeft: 8}}
+              contentInset={{left: -8}}
+              horizontal
+              showsHorizontalScrollIndicator={false}>
+              {berita.map((e, index) => (
+                <Berita key={index} props={e} />
+              ))}
+            </ScrollView>
+          </View>
+        </ScrollView>
       </View>
     </ImageBackground>
   );
@@ -64,7 +91,7 @@ var styles = StyleSheet.create({
   },
   mainPar: {
     padding: ratios.widthPixel(32),
-    height: ratios.heightPixel(127),
+    height: ratios.heightPixel(134),
     width: '100%',
   },
   mainHead: {
@@ -83,14 +110,16 @@ var styles = StyleSheet.create({
   },
   tickSeyPar: {
     marginVertical: ratios.widthPixel(16),
-    marginLeft: ratios.widthPixel(20),
+  },
+  beritaPar: {
+    marginTop: ratios.widthPixel(-16),
   },
   tickSey: {
+    marginLeft: ratios.widthPixel(20),
     fontFamily: fonts.JakBol,
     fontSize: ratios.fontPixel(15),
     color: colors.darkGray,
     padding: ratios.widthPixel(12),
     paddingTop: 0,
   },
-  ticketCarPar: {},
 });
