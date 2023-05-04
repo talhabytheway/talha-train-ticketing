0130093bbc86abb6def4ecc58a34ef97618597b4
Muhammad Talha <talhahanfsd@gmail.com>
2023-05-04 22:16:38 +0500

    Design PassengerDetails Comp of Summary Screen

---
+++ src/components/summary/PassangerDetails.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const PassangerDetails = () => {
+  return (
+    <View>
+      <Text>PassangerDetails</Text>
+    </View>
+  );
+};
+
+export default PassangerDetails;
+
+const styles = StyleSheet.create({});
