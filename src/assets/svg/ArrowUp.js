853d5d9163bb0cece5894de8d78a5206dd0371a5
Muhammad Talha <talhahanfsd@gmail.com>
2023-05-04 23:38:46 +0500

    Add Functionality of Dimensions

--- src/assets/svg/ArrowUp.js
+++ src/assets/svg/ArrowUp.js
@@ -2,13 +2,14 @@ import * as React from 'react';
 import Svg, {Path} from 'react-native-svg';
 import ratios from '../../constants/ratios';
 
-const SvgComponent = props => (
+const SvgComponent = ({dimensions = 20, style}) => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
-    width={ratios.widthPixel(20)}
-    height={ratios.widthPixel(20)}
-    fill="none"
-    {...props}>
+    width={ratios.widthPixel(dimensions)}
+    height={ratios.widthPixel(dimensions)}
+    style={style}
+    viewBox="0 0 20 20"
+    fill="none">
     <Path
       fill="#88879C"
       fillRule="evenodd"
